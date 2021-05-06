3031fd8012f52fb9c598922374a318c90df4b654
Antonio Cituk <tonycituk@gmail.com>
2021-05-06 02:57:08 -0500

    06-05-2021 02:57

--- backup.sh
+++ backup.sh
@@ -1,21 +1,21 @@
 clear
 cowsay "Backing up caranalga server 10%"
-sleep .5
+sleep 1
 rm -f ./commitMessage.txt
 date +"%d-%m-%Y %H:%M" >> commitMessage.txt
 git add .
 clear
 cowsay "Backing up caranalga server 30%"
-sleep .5
+sleep 1
 git status
 git commit -F commitMessage.txt
 clear
 cowsay "Backing up caranalga server 70%"
-sleep .5
+sleep 1
 git push -u origin main
 clear
 cowsay "Backing up caranalga server 95%"
-sleep .5
+sleep 1
 rm -f ./commitMessage.txt
 clear
 cowsay "Backed up caranalga server 🧳💗"
